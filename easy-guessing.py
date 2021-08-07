ba0defac1defdf9c4534163206d08f4e8f4797d5
sql-inject0r <88199578+sql-inject0r@users.noreply.github.com>
2021-08-07 11:08:34 +0530

    Update easy-guessing.py

--- easy-guessing.py
+++ easy-guessing.py
@@ -1,17 +1,17 @@
 # a very basic guessing game for begginers learning if and else statements !
-answer = 5
+import random
+highest = 10
+answer = random.randint(1,10)
+print(answer) # TODO:remove after testing
+guess = int(input("Please enter a number between 1 and {}: ".format(highest)))
 
-guess = int(input("Please enter a number between 1 and 10: "))
-
-if guess != answer:
-    if guess > answer:
-        print("Please guess lower !")
-    else:
-        print("Please guess higher !")
-    guess = int(input("Please enter a number between 1 and 10: "))
+while guess != answer:
+    guess =int(input("enter a number between 1 and {}: ".format(highest)))
     if guess == answer:
-        print("yay you have guessed it correctly :D")
-    else:
-        print("sorry you have not guessed it correctly :(")
-else:
-    print("YAY you got it first time :DDD")
+        print("you got it :D")
+        break
+    else: # nested if else condition 
+        if guess > answer:
+            print("Please guess lower !")
+        else:
+            print("please guess higher !")
